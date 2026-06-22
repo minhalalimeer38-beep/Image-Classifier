4a4d5b8591a2dbd7c10d3bb15099ad45b6a44f0c
minhalalimeer38-beep <minhalalimeer38@gmail.com>
2026-06-22 22:26:21 +0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,7 @@ if file:
 
     if st.button("Predict"):
         response = requests.post(
-            "https://minhalali12-image-classifier.hf.space/docs#/default/predict_predict_post",
+            "https://minhalali12-image-classifier.hf.space/predict",
             files={"file": file.getvalue()}
         )
 
